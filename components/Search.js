80aa60c3caa55f6d31423ef0d85f8eb081d3e2c7
Ziyad-Aljunaidi <ziyad.al.junaidi@gmail.com>
2022-03-08 01:47:02 +0200

    still carousel tweaking :)

--- components/Search.js
+++ components/Search.js
@@ -14,7 +14,7 @@ export default function Search() {
               aria-describedby="button-addon2"
             />
             <button
-              className="btn inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-r shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out items-center"
+              className="btn inline-block px-6 py-2.5 bg-blue-600 border-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-r shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700  focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out items-center"
               type="button"
               id="button-addon2"
             >
